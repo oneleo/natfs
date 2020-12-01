ccd7a398b5d153a4348758cd425d2aab88ae6946
oneleo <oneleo760823@gmail.com>
2020-12-02 00:57:10 +0800

    Update fs.cmd

--- fs.cmd
+++ fs.cmd
@@ -6,14 +6,13 @@ netsh advfirewall firewall add rule name="Port 81" dir=in action=allow protocol=
 choco install git.install dontsleep.install golang -y
 ::refreshenv
 md %USERPROFILE%\go\src\github.com\codeskyblue
-::"%SystemDrive%\Go\bin\go.exe" get github.com/codeskyblue/gohttpserver
+md %USERPROFILE%\go\src\github.com\oneleo
 cd /d %USERPROFILE%\go\src\github.com\codeskyblue
 "%ProgramFiles%\Git\cmd\git.exe" clone https://github.com/codeskyblue/gohttpserver.git
+cd /d %USERPROFILE%\go\src\github.com\oneleo
 "%ProgramFiles%\Git\cmd\git.exe" clone https://github.com/oneleo/natfs.git
 cd /d %USERPROFILE%\go\src\github.com\codeskyblue\gohttpserver
 :: Predict Drive Letter
-::"%SystemDrive%\Go\bin\go.exe" build && .\gohttpserver --port 80 -r "D:"
-::"%SystemDrive%\Go\bin\go.exe" install && "%USERPROFILE%\go\bin\gohttpserver" --port 80 -r "D:"
 "%SystemDrive%\Go\bin\go.exe" install
 copy "%USERPROFILE%\go\src\github.com\oneleo\natfs\invisible.vbs" "%USERPROFILE%\go\bin\"
 "%windir%\System32\wscript.exe" "%USERPROFILE%\go\bin\invisible.vbs" "%USERPROFILE%\go\bin\gohttpserver" --port 80 -r "D:"
